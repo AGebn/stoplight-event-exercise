2a9dbcb7576dc0175f807c16b4f440b5ce146e4a
AGebn <adam.gebner@protonmail.com>
2023-03-03 12:23:07 -0800

    first commit (lights functional

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-(function() {
-  'use strict';
+var stopButton = document.getElementById("stopButton");
+var slowButton = document.getElementById("slowButton");
+var goButton = document.getElementById("goButton");
+var stopLight = document.getElementById("stopLight");
+var slowLight = document.getElementById("slowLight");
+var goLight = document.getElementById("goLight");
 
-  // YOUR CODE HERE
-})();
+// console.log(slowButton);
+
+var stopOn = false;
+var slowOn = false;
+var goOn = false;
+
+stopButton.addEventListener("click", function () {
+  if (stopOn === false) {
+    stopLight.style.backgroundColor = "red";
+    stopOn = true;
+    return stopOn;
+  } else {
+    stopLight.style.backgroundColor = "black";
+    stopOn = false;
+    return stopOn;
+  }
+});
+
+slowButton.addEventListener("click", function () {
+  if (slowOn === false) {
+    slowLight.style.backgroundColor = "orange";
+    slowOn = true;
+    return slowOn;
+  } else {
+    slowLight.style.backgroundColor = "black";
+    slowOn = false;
+    return slowOn;
+  }
+});
+
+goButton.addEventListener("click", function () {
+  if (goOn === false) {
+    goLight.style.backgroundColor = "green";
+    goOn = true;
+    return goOn;
+  } else {
+    goLight.style.backgroundColor = "black";
+    goOn = false;
+    return goOn;
+  }
+});
